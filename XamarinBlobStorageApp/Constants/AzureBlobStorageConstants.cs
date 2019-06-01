2b06a3e02f4da611ab2fc5c0a3ea2c70c9efb11d
Jacek Kashiash Kosiński <kashiash@gmail.com>
2019-06-01 12:09:42 +0200

    .

--- XamarinBlobStorageApp/Constants/AzureBlobStorageConstants.cs
+++ XamarinBlobStorageApp/Constants/AzureBlobStorageConstants.cs
@@ -4,6 +4,7 @@
     {
      //#error Missing Blob Storage Connection String
     //Create an Azure Blob Storage instance here: https://aka.ms/AzureBlobStorage
+    
 		public const string ConnectionString = "DefaultEndpointsProtocol = https; AccountName=zdjeciapato;AccountKey=ZCF/XGgIcbf9uoi4UfJAcKACL4O8yM6220XqBcGvn8doAuhe+Az2Dq9eGnGOg8w+U1S87Z7HMwk6EPD6mcFSlw==;EndpointSuffix=core.windows.net";
         public const string ContainerName = "zdjecia";
     }
